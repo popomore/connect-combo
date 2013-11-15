5b1a76ce805f3f97588d2300e196553a71769911
popomore <sakura9515@gmail.com>
2013-11-15 12:44:38 +0800

    refactor

--- index.js
+++ index.js
@@ -1,21 +1,17 @@
 var path = require('path');
-var fs = require('fs');
 var async = require('async');
 var format = require('util').format;
-var url = require('url');
 var mime = require('mime');
-var concat = require('concat-stream');
-var protocol = {
-  http: require('http'),
-  https: require('https')
-};
+var log = require('./lib/log');
+var File = require('./lib/file');
+
 
 var defaults = {
   // local directory
   directory: process.cwd(),
   
   // remote server
-  proxy: false,
+  proxy: '',
 
   // cache remote file
   cache: false,
@@ -28,69 +24,18 @@ module.exports = function combo(options) {
 
   options = extend(defaults, options);
 
-  function getRemote(u, callback) {
-    var parsed = url.parse(u);
-    parsed.agent = false;
-    parsed.method = 'GET';
-  
-    // don't check ssl
-    parsed.secureOptions = require('constants').SSL_OP_NO_TLSv1_2;
-    parsed.rejectUnauthorized = false;
-  
-    var request = protocol[parsed.protocol.replace(':', '')];
-  
-    var req = request.get(parsed, function(res) {
-      if (res.statusCode !== 200) {
-        log('Not Found ' + u);
-        callback(new Error(res.statusCode));
-        return;
-      }
-      res.pipe(concat(function(data) {
-        log('Found ' + u);
-        if (options.cache) {
-          var file = path.join(options.directory, parsed.path);
-          writeFile(file, data, function(err) {
-            !err && log('Cached ' + u);
-          });
-        }
-        callback(null, data);
-      }));
-    }).on('error', function(e) {
-      log('Not Found ' + u);
-      callback(e);
-    });
-  }
-
-  function log(str) {
-    options.log && console.log('>> ' + str);
-  }
-
   return function(req, res, next) {
     var files = normalize(req.url);
     if (files && files.length) {
-      log('Request ' + req.url);
+      log('Request ' + req.url, options);
       var exts = getExt(files);
       if (exts.length !== 1) {
         res.writeHead(400, {'Content-Type': 'text/html'});
         res.end('400 Bad Request');
       } else {
         async.map(files, function(item, done) {
-          var filePath = path.join(options.directory, item);
-          fs.readFile(filePath, function(err, data) {
-            if (!err) {
-              // find file from directory
-              log('Found ' + filePath);
-              done(null, data.toString());
-            } else {
-              log('Not Found ' + filePath);
-              if (options.proxy) {
-                // find file from remote server
-                getRemote(url.resolve(options.proxy, item), done);
-              } else {
-                // not found
-                done(err);
-              }
-            }
+          new File(item, options).end(function(err, data) {
+            done(err, data);
           });
         }, function(err, results){
           if (err) {
@@ -107,7 +52,6 @@ module.exports = function combo(options) {
       next();
     }
   };
-
 };
 
 // '/a??b.js,c/d.js' => ['a/b.js', 'a/c/d.js']
@@ -135,15 +79,6 @@ function getExt(files) {
   return Array.isArray(files) ? files : [files];
 }
 
-function writeFile(filePath, data, cb) {
-  var dirname = path.dirname(filePath);
-  var dirs = dirname.split('/');
-  for (var i = 2; i <= dirs.length; i++) {
-    var p = dirs.slice(0, i).join('/');
-    !fs.existsSync(p) && fs.mkdirSync(p);
-  }
-  fs.writeFile(filePath, data, cb);
-}
 
 function extend(target, src) {
   var result = {};
